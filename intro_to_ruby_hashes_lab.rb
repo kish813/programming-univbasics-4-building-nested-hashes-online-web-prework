d2975c81ce690c63d6d1c3f71d040442558dfdfb
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-28 17:54:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,10 +6,9 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  person = {
-    name: "Bob"
-    age: "31"
-  }
+  person =>
+    {name: "Bob", age: "31"}
+    {name: "Ken", age: "30"}
   
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
